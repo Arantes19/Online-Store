2737e2c34a576612ace814feef6d84b64c489bba
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-12-03 21:48:40 +0000

    N-tier design update

---
+++ onlinestore/BusinessObjects/Payment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    /// <summary>
+    /// Represents the payment 
+    /// </summary>
+    public class Payment
+    {
+        #region Properties
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+        #endregion
+
+        #endregion
+    }
+}
---
+++ onlinestore/BusinessRules/ManageStore.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessRules
+{
+    public class ManageStore
+    {
+
+    }
+}
