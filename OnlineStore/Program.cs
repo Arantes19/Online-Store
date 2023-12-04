f9301628f8831c115f9684d29b6ffc8ae15d1933
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-12-04 22:39:05 +0000

    N-tier design

---
+++ OnlineStore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
